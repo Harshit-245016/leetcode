8c360bef6af3feed9e00936914b709c074a8c445
Harshit-245016 <harshitsharma245016@gmail.com>
2026-08-12 00:41:12 +0530

    Time: 1438 ms (9.13%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,1 +1,5 @@
-select e.name,b.bonus from employee as e left join bonus as b on e.empid=b.empid where b.bonus<1000 or b.bonus Is Null;
+select e.name,b.bonus 
+from employee as e
+ left join bonus as b
+on e.empid=b.empid 
+where b.bonus<1000 or b.bonus Is Null;
